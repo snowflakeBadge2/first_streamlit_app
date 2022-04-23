9495c527c6a34d8c01884ffd07a2a0c7e45cf2f4
snowflakeBadge2 <104251806+snowflakeBadge2@users.noreply.github.com>
2022-04-23 14:15:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('Snowflake Badge 2 Diner')
+
+streamlit.header('Menu')
+
+streamlit.text('Waffle')
+streamlit.text('Shrimp')
+streamlit.text('Taco')
+streamlit.text('Momo')
