3c4e1c2e2261e6b6ad288d71dcc97b3bbc764336
snowflakeBadge2 <104251806+snowflakeBadge2@users.noreply.github.com>
2022-04-24 18:07:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,13 @@ import snowflake.connector
 from urllib.error import URLError
 
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+    fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
+
+
 streamlit.title('Snowflake Badge 2 Diner')
 
 
@@ -34,9 +41,8 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+   back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 
 except URLError as e:
   streamlit.error()
